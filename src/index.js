803aad120e345e4741efdfd87d2d5d693469abf0
lontubu <nickson.lontubu@studentmoringa.com>
2023-10-03 14:40:34 +0300

    Comleted assignments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form= document.querySelector("form")
+  form.addEventListener('submit',(e)=> {
+    e.preventDefault()
+    list(e.target["new-task-description"].value)
+    form.reset()
+})
+
+}); 
+
+function list(todo){
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener("click",(e)=>{ 
+    e.target.parentNode.remove()
+    
+})
+li.textContent='${todo}'
+document.querySelector("ul").appendChild(li)
+
+li.appendChild(btn)
+btn.textContent="X"
+  
+}
